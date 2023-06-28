cf84aabc21bc596ed8db76344e41361016ba7c5c
Evan Gabrielson <ejgabrie@usc.edu>
2023-06-28 16:22:09 -0700

    initial commit

--- lib/carbonlink-api-stack.ts
+++ lib/carbonlink-api-stack.ts
@@ -1,4 +1,5 @@
 import * as cdk from 'aws-cdk-lib';
+import { ShellStep, CodePipeline, CodePipelineSource } from 'aws-cdk-lib/pipelines';
 import { Construct } from 'constructs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
@@ -6,11 +7,14 @@ export class CarbonlinkApiStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'CarbonlinkApiQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    new CodePipeline(this, 'Pipeline', {
+      pipelineName: 'TestPipeline',
+      synth: new ShellStep('Synth', {
+        input: CodePipelineSource.connection('evangabe/carbonlink-api', 'master', {
+          connectionArn: 'arn:aws:codestar:us-east-1:417916115807:project/api-testproject'
+        }),
+        commands: ['npm ci', 'npm run build', 'npx cdk synth']
+      })
+    })
   }
 }
