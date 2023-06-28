6e19f6312cae9a2ce93faa7b99c4389c46ad900e
Evan Gabrielson <ejgabrie@usc.edu>
2023-06-28 15:52:03 -0700

    Initial commit

---
+++ test/carbonlink-api.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CarbonlinkApi from '../lib/carbonlink-api-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/carbonlink-api-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CarbonlinkApi.CarbonlinkApiStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
