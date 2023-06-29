556c1e2f42ff44508e21f64c8e194d670cbaee7d
Evan Gabrielson <ejgabrie@usc.edu>
2023-06-28 22:43:32 -0700

    constructified single table

--- lib/constructs/stateful/OneTable.ts
+++ lib/constructs/stateful/OneTable.ts
@@ -3,6 +3,7 @@ import { AttributeType, BillingMode, Table } from "aws-cdk-lib/aws-dynamodb";
 import { Construct } from "constructs";
 
 export class OneTable extends Construct {
+    public readonly table: Table;
     constructor(scope: Construct, id: string) {
         super(scope, id);
 
@@ -44,5 +45,7 @@ export class OneTable extends Construct {
                 type: AttributeType.STRING
             }
         });
+
+        this.table = table;
     }
 }
--- lib/stateful-stack.ts
+++ lib/stateful-stack.ts
@@ -1,13 +1,14 @@
 import { Stack, StackProps } from "aws-cdk-lib";
 import { Construct } from "constructs";
 import { OneTable } from "./constructs/stateful/OneTable";
+import { Table } from "aws-cdk-lib/aws-dynamodb";
 
 export class ApiStatefulStack extends Stack {
-    public readonly coreTable: OneTable;
+    public readonly coreTable: Table;
     constructor(scope: Construct, id: string, stageName: string, props?: StackProps) {
         super(scope, id, props);
         
         // Implementing single-table design with a Core Table
-        this.coreTable = new OneTable(this, "CoreTable");
+        this.coreTable = new OneTable(this, "CoreTable").table;
     }
 }
